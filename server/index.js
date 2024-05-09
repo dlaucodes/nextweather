040d3b2f80b4582c34876ba6432229f60281984a
Chun Lau <dlaucodes@gmail.com>
2024-05-09 17:58:26 -0400

    add notes

--- server/index.js
+++ server/index.js
@@ -15,10 +15,13 @@ const API_KEY = process.env.WEATHER_API_KEY;
 const WEATHER_BY_CITY_URL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_KEY}`;
 
 // helpers
+
+//takes lat, lon and creates the url
 const buildWeatherByCityURL = (lat, lon) => {
   return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}`;
 };
 
+//takes city to request for geolocation data to be broken down into lat lon
 const buildGeolocationURL = (
   cityName,
   stateCode = "",
@@ -30,14 +33,14 @@ const buildGeolocationURL = (
   }
   return `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${API_KEY}`;
 };
-
+//used for getting lat, lon from city
 const makeGeoLocationRequest = async (city) => {
   const geoLocationURL = buildGeolocationURL(city);
   const geoLocationResp = await fetch(geoLocationURL);
   const geoLocationData = await geoLocationResp.json();
   return geoLocationData;
 };
-
+//processing deconstucted lat, lon from makeGeoLocationRequest
 const makeWeatherRequest = async (lat, lon) => {
   const weatherURL = buildWeatherByCityURL(lat, lon);
   const weatherResp = await fetch(weatherURL);
@@ -61,9 +64,11 @@ app.get("/weather/city", async (req, res) => {
     throw new Error("No locations were found for this city");
   }
 
-  //pull out first item in data arary since it reurns an array of objects
+  //pull out first item in data array since it returns an array of objects
   const cityData = geoLocationData[0];
+  //deconstructed and saved into cityData
   const { lat, lon } = cityData;
+
 
   const weatherData = await makeWeatherRequest(lat, lon);
 
